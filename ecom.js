c692fb25d7f2ee01bd9afea778adb5126f1f979b
ARKing130310 <asimasimazam6@gmail.com>
2025-09-26 12:15:24 +0100

    Add files via upload

---
+++ ecom.js
@@ -0,0 +1,12 @@
+// Register Form JS
+const regForm = document.getElementById("Register-form");
+
+regForm.addEventListener("click", () =>{
+    regForm = window.location.href = 'Register.html';
+});
+// Submit Button 
+const subBtn = document.getElementById("submit-btn");
+
+subBtn.addEventListener("click", () => {
+    subBtn = window.location.href = 'ecom.html';
+});
